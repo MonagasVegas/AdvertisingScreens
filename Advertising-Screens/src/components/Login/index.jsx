4d7bec8d5a6eba98e50df2e5a7ee63d2f1660ff1
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-04-30 18:14:16 -0300

    feat: login construction started.

--- Advertising-Screens/src/components/Login/index.jsx
+++ Advertising-Screens/src/components/Login/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import "./index.css";
 
 const Login = () => {
   return (
-    <div>
-        <h1>Prueba.</h1>
-    </div>
-  )
-}
+    <div className="container">
 
-export default Login
+      <div className="card">
+        <form>
+          <div className="form">
+            <div style={{ alignItems: 'center', textAlign: 'center', marginBottom: '20px'}}>
+              <h1>Sign In</h1>
+              <span>
+                Dont have an account?{" "}
+                <span className="underlinedText">Sign Up</span>
+              </span>
+            </div>
+            <div className="input-box">
+              <input />
+              <label>Correo</label>
+              <input />
+              <label>Correo</label>
+            </div>
+            <div style={{ alignItems: 'center', textAlign: 'center', marginBottom: '20px'}} >
+              <span className="underlinedText">Forgot your password?</span>
+            </div>
+            <div style={{ alignItems: 'center', textAlign: 'center'}}>
+              <button className="button" type="submit">
+                Sign in
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
