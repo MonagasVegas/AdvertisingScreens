3bdbd20de4bea6408882af5d6a589f639a516d94
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-05-01 17:20:36 -0300

    feat: color settings.

---
+++ Advertising-Screens/src/components/Search/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import SearchIcon from '../../assets/svg/SearchIcon.svg'
+import { colors } from '../../styles/Colors'
+
+const SearchBar = () => {
+  return (
+    <div>
+
+<div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+        <img src={SearchIcon} />
+      </div>
+
+      <form
+        action='#'
+        autoComplete='off'
+        style={{
+          display: 'flex',
+          width: '100%'
+        }}
+      >
+        <input
+          name={'search' + id}
+          className='outline-none  block pl-10 w-full text-xs rounded-lg border placeholder-gray-400 placeholder-sm text-gray-400'
+          type='search'
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          autoComplete='off'
+          style={{ backgroundColor: colors.Grey100 }}
+        />
+      </form>
+    </div>
+  )
+}
+
+export default SearchBar
---
+++ Advertising-Screens/src/styles/Colors.js
@@ -0,0 +1,17 @@
+export const colors = {
+    blue50: '#E3F2FD',
+    Grey50: '#FAFAFA',
+    Grey100: '#F5F5F5',
+    Grey200: '#EEEEEE',
+    Grey300: '#E0E0E0',
+    Grey500: '#9E9E9E',
+    pureWhite: '#FFFFFF',
+    dark: "#1C1C1C",
+    semiDark: "#282828",
+    light: "#2A303C",
+    grayDark: '#3A3A3A',
+    turquoise: "#49BFBF",
+    primary: "#554F95",
+    secondary: "#91c870",
+  }
+  
