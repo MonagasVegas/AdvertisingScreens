7b6225144cbe8cd9ebabd428eaab1886e0dc62e4
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-05-01 11:55:39 -0300

    feat: home initialization.

---
+++ Advertising-Screens/src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
