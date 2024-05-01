c7ec13ee1de165a61de6715d4bfacf8612e0cccc
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-04-30 21:04:22 -0300

    feat: construction design login.

--- Advertising-Screens/src/components/Login/index.jsx
+++ Advertising-Screens/src/components/Login/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import { useForm } from "react-hook-form";
+import city from "../../assets/images/pu.png";
 
 const Login = () => {
   const {
@@ -9,7 +10,7 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  console.log('ERROR', errors)
+  console.log("ERROR", errors);
 
   const onSubmit = handleSubmit((data) => {
     console.log("data", data);
@@ -18,68 +19,84 @@ const Login = () => {
 
   return (
     <div className="container">
-      <div className="card">
-        <form onSubmit={onSubmit}>
-          <div className="form">
-            <div style={{ alignItems: "center", textAlign: "center", marginBottom: "20px"}}>
-              <h1>Sign In</h1>
-              <span>
-                Dont have an account?{" "}
-                <span className="underlinedText">Sign Up</span>
-              </span>
-            </div>
+        <div className="card">
+          <form onSubmit={onSubmit}>
+            <div className="form">
+              <div
+                style={{
+                  alignItems: "center",
+                  textAlign: "center",
+                  marginBottom: "20px",
+                }}
+              >
+                <h1>Sign In</h1>
+                <span>
+                  Dont have an account?{" "}
+                  <span className="underlinedText">Sign Up</span>
+                </span>
+              </div>
 
-            <div className="input-field">
-              <label>Email</label>
-              <input
-                type="text"
-                {...register("email", {
-                  required: {
-                    value: true,
-                    message:
-                      "Campo requerido, debe ingresar un correo electrónico",
-                  },
-                  pattern: {
-                    value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                    message: "Correo no válido",
-                  },
-                })}
-              />
-              {errors.email && (
-                <p className="error">
-                  {errors.email.message}
-                </p>
-              )}
-            </div>
-            <br />
-            <div className="input-field">
-              <label>Password</label>
-              <input
-                type="text"
-                {...register("password", {
-                  required: {
-                    value: true,
-                    message: "Campo requerido, debe ingresar una contraseña",
-                  }
-                })}
-              />
-              {errors.password && (
-                <p className="error">
-                  {errors.password.message}
-                </p>
-              )}
-            </div>
+              <div className="input-field">
+                <label>Email</label>
+                <input
+                  type="text"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message:
+                        "Campo requerido, debe ingresar un correo electrónico",
+                    },
+                    pattern: {
+                      value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                      message: "Correo no válido",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <p className="error">{errors.email.message}</p>
+                )}
+              </div>
+              <br />
+              <div className="input-field">
+                <label>Password</label>
+                <input
+                  type="text"
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "Campo requerido, debe ingresar una contraseña",
+                    },
+                  })}
+                />
+                {errors.password && (
+                  <p className="error">{errors.password.message}</p>
+                )}
+              </div>
 
-            <div style={{ alignItems: "center",  textAlign: "center", marginBottom: "20px", marginTop: "20px" }}>
-              <span className="underlinedText">Forgot your password?</span>
+              <div
+                style={{
+                  alignItems: "center",
+                  textAlign: "center",
+                  marginBottom: "20px",
+                  marginTop: "20px",
+                }}
+              >
+                <span className="underlinedText">Forgot your password?</span>
+              </div>
+              <div
+                style={{
+                  alignItems: "center",
+                  textAlign: "center",
+                  marginBottom: "20px",
+                }}
+              >
+                <button className="button" type="submit">
+                  Sign in
+                </button>
+              </div>
             </div>
-            <div style={{ alignItems: "center", textAlign: "center", marginBottom: "20px" }}>
-              <button className="button" type="submit">
-                Sign in
-              </button>
-            </div>
-          </div>
-        </form>
+          </form>
+        
       </div>
     </div>
   );
