fcdf0ef2021bd3a6c814b980c7f9a82b68eeb866
MonagasVegas <35318244+MonagasVegas@users.noreply.github.com>
2024-05-04 16:04:07 -0300

    feat: add component

--- Advertising-Screens/src/App.jsx
+++ Advertising-Screens/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import "./index.css"
 import Login from "./components/Login";
 import Home from "./components/Home";
+import CreateScreen from "./components/CreateScreen/CreateScreen";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/createScreen' element={<CreateScreen />} />
       </Routes>
     </HashRouter>
     </>
